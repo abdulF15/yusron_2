7e7011ad4d5f9d3791dee257956f921df6c50487
abdulf15 <aafattahh@gmail.com>
2025-05-20 20:34:13 +0700

    rev3

--- source/app.js
+++ source/app.js
@@ -45,7 +45,7 @@ navLinks.forEach((link) => {
 
     // jika login arahkan ke halaman login
     if (link.classList.contains("login")) {
-      window.location.href = "/pages/login.html";
+      window.location.href = "yusron_2/login.html";
     }
 
     setTimeout(() => {
